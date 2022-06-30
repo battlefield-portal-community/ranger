fe705021c348ac59ef6d4259e1c56cd911a3c79f
p0lygun <solankivibhakar82@gmail.com>
2022-06-30 14:59:56 +0530

    refactor: move reused code to wrapper functions

--- bot/dashboard/dashboard.py
+++ bot/dashboard/dashboard.py
@@ -1,9 +1,13 @@
 import json
 import os
 import asyncio
+from pathlib import Path
+
+import jsonschema
 import psycopg2
+from jsonschema import validate
 from loguru import logger
-from ..utils.helper import project_base_path
+from ..utils.helper import project_base_path, configs_path
 
 try:
     from ..pgsql import ConnectionWrapper
@@ -42,26 +46,26 @@ try:
 
         @app.get("/", response_class=HTMLResponse)
         async def read_root(request: Request):
-            with open(project_base_path / "configs" / "schemas" / "global_config.schema.json") as file:
-                schema = json.load(file)
-            with open(project_base_path / "configs" / "global_config.json") as file:
-                config = json.load(file)
-            return templates.TemplateResponse(
-                "index.html",
-                {
-                    'request': request,
-                    'schema': schema,
-                    'start_val': config
-                }
-            )
+            if files := await get_files('global_config'):
+                with open(files[0]) as config_file, open(files[1]) as schema_file:
+                    config, schema = json.load(config_file), json.load(schema_file)
+
+                return templates.TemplateResponse(
+                    "index.html",
+                    {
+                        'request': request,
+                        'schema': schema,
+                        'start_val': config
+                    }
+                )
 
 
         @app.get('/configs/', response_class=HTMLResponse)
         async def return_config(request: Request, config: str):
-            with open(project_base_path / "configs" / "schemas" / f"{config}.schema.json") as file:
-                schema = json.load(file)
-            with open(project_base_path / "configs" / f"{config}.json") as file:
-                saved_config = json.load(file)
+            if files := await get_files(config):
+                with open(files[0]) as config_file, open(files[1]) as schema_file:
+                    saved_config, schema = json.load(config_file), json.load(schema_file)
+
             return templates.TemplateResponse(
                 "config_editor.html",
                 {
@@ -71,24 +75,22 @@ try:
                 }
             )
 
+        async def return_file(config_name: str, config: bool = False, schema: bool = False) -> dict:
+            if config or schema:
+                if files := await get_files(config_name):
+                    with open(files[0 if config else 1]) as file:
+                        return json.load(file)
+                else:
+                    return {config: 'Invalid'}
+
         @app.get('/raw/configs')
         async def return_config(config: str):
-            config_folder = project_base_path / "configs"
-            if config in [json_file.stem for json_file in list(config_folder.glob("*.json"))]:
-                with open(config_folder / f"{config}.json") as file:
-                    return json.load(file)
-            else:
-                return {config: 'Invalid'}
+            return await return_file(config, config=True)
 
 
         @app.get('/raw/configs/schema')
-        async def return_config(config: str):
-            config_schema_folder = project_base_path / "configs" / "schemas"
-            if f"{config}.schema" in [json_file.stem for json_file in list(config_schema_folder.glob("*.json"))]:
-                with open(config_schema_folder / f"{config}.schema.json") as file:
-                    return json.load(file)
-            else:
-                return {config: 'Invalid'}
+        async def return_schema(config: str) -> dict:
+            return await return_file(config, schema=True)
 
 
         @app.get("/get/channel/")
@@ -106,13 +108,20 @@ try:
 
         @app.post("/post/", status_code=201)
         async def save_config(request: Request, config: str):
-            config_folder = project_base_path / "configs"
-            if config in [json_file.stem for json_file in list(config_folder.glob("*.json"))]:
-                with open(config_folder / f"{config}.json", 'w') as file:
+            if files := await get_files(config):
+                with open(files[0], 'w') as file:
                     json.dump(await request.json(), file, indent=2)
                     return {"saved": True}
             else:
                 return {"saved": False, "config": False}
+
+        async def get_files(config_name: str) -> tuple[Path, Path] | bool:
+            schema_files = [config_file for config_file in list((configs_path / "schemas").glob("*.schema.json"))]
+            if f"{config_name}.schema" in [file.stem for file in schema_files]:
+                return configs_path / f"{config_name}.json", configs_path / "schemas" / f"{config_name}.schema.json"
+            else:
+                return False
+
 
 except ConnectionError as e:
     logger.critical(f"Unable to connect to Discord. exit error {e}")
--- bot/utils/helper.py
+++ bot/utils/helper.py
@@ -2,7 +2,7 @@ import pathlib
 from random import choice
 
 project_base_path = pathlib.Path(__file__).parents[1]
-
+configs_path = project_base_path / "configs"
 
 def random_emoji() -> str:
     return choice(
