032570503a1e1c218bd227eaf03d1c3fc7a8e4ed
p0lygun <solankivibhakar82@gmail.com>
2022-06-30 12:08:35 +0530

    feat: complete dashboad static file serve

--- bot/dashboard/dashboard.py
+++ bot/dashboard/dashboard.py
@@ -15,10 +15,18 @@ except psycopg2.OperationalError:
 
 try:
     if not os.getenv('DB_DEBUG', None):
-        from fastapi import FastAPI
+        from fastapi import FastAPI, Request
+        from fastapi.responses import HTMLResponse
+        from fastapi.staticfiles import StaticFiles
+        from fastapi.templating import Jinja2Templates
+
         from bot.bot import Ranger
 
         app = FastAPI()
+        dashboard_root = project_base_path / "dashboard"
+        static_path = dashboard_root / "static"
+        app.mount(str(static_path), StaticFiles(directory=static_path), name="static")
+        templates = Jinja2Templates(directory=dashboard_root / "templates")
 
         bot = Ranger(debug_guilds=[
             int(os.getenv('GUILD_ID'))
@@ -32,11 +40,38 @@ try:
             # asyncio.create_task(bot.start(os.getenv("DISCORD_TOKEN")))
 
 
-        @app.get("/")
-        async def read_root():
-            return {"Hello": str(bot.user)}
+        @app.get("/", response_class=HTMLResponse)
+        async def read_root(request: Request):
+            with open(project_base_path / "configs" / "schemas" / "global_config.schema.json") as file:
+                schema = json.load(file)
+            with open(project_base_path / "configs" / "global_config.json") as file:
+                config = json.load(file)
+            return templates.TemplateResponse(
+                "index.html",
+                {
+                    'request': request,
+                    'schema': schema,
+                    'start_val': config
+                }
+            )
 
-        @app.get('/configs/get/')
+
+        @app.get('/configs/', response_class=HTMLResponse)
+        async def return_config(request: Request, config: str):
+            with open(project_base_path / "configs" / "schemas" / f"{config}.schema.json") as file:
+                schema = json.load(file)
+            with open(project_base_path / "configs" / f"{config}.json") as file:
+                saved_config = json.load(file)
+            return templates.TemplateResponse(
+                "config_editor.html",
+                {
+                    'request': request,
+                    'schema': schema,
+                    'start_val': saved_config
+                }
+            )
+
+        @app.get('/raw/configs')
         async def return_config(config: str):
             config_folder = project_base_path / "configs"
             if config in [json_file.stem for json_file in list(config_folder.glob("*.json"))]:
@@ -45,7 +80,8 @@ try:
             else:
                 return {config: 'Invalid'}
 
-        @app.get('/configs/get/schema')
+
+        @app.get('/raw/configs/schema')
         async def return_config(config: str):
             config_schema_folder = project_base_path / "configs" / "schemas"
             if f"{config}.schema" in [json_file.stem for json_file in list(config_schema_folder.glob("*.json"))]:
@@ -53,6 +89,7 @@ try:
                     return json.load(file)
             else:
                 return {config: 'Invalid'}
+
 
         @app.get("/get/channel/")
         async def channel_name(id: int):
