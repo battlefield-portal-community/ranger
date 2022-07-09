e065001da2cf9367f7c0bbf607b01e43fa2e1706
p0lygun <solankivibhakar82@gmail.com>
2022-07-09 22:04:34 +0530

    feat: handle edge case when no channels in config

--- bot/cogs/embed_message.py
+++ bot/cogs/embed_message.py
@@ -43,52 +43,54 @@ class EmbedMessageManager(CogBase):
             if self.bot.config['cogs']['embed_message']['enabled']:
                 if self.config:
                     for channel_, a_channel in zip_longest(self.config['channels'], self.applied_config.get("channels", [])):
-                        if channel := self.bot.get_channel(int(channel_['id'])):
-                            channel_['name'] = channel.name
-                            embed: dict
-                            for message, a_message in zip_longest(
-                                    channel_['messages'], a_channel['messages'] if a_channel else []):
-                                try:
-                                    msg = await channel.get_partial_message(int(message['id'])).fetch() if message[
-                                        'id'] else None
-                                except discord.NotFound or discord.Forbidden:
-                                    msg = None
+                        if channel_:
+                            if channel := self.bot.get_channel(int(channel_['id'])):
+                                channel_['name'] = channel.name
+                                embed: dict
+                                for message, a_message in zip_longest(
+                                        channel_['messages'], a_channel['messages'] if a_channel else []):
+                                    try:
+                                        msg = await channel.get_partial_message(int(message['id'])).fetch() if message[
+                                            'id'] else None
+                                    except discord.NotFound or discord.Forbidden:
+                                        msg = None
 
-                                if any(map(len, [message['content'], message['embeds']])) and (
-                                        message != a_message or msg is None):
+                                    if any(map(len, [message['content'], message['embeds']])) and (
+                                            message != a_message or msg is None):
 
-                                    embeds = []
-                                    for embed in message['embeds']:
-                                        if len(embed):
-                                            kwargs = dict()
-                                            for key, value in embed.items():
-                                                if not len(value):
-                                                    if key == "description":
-                                                        kwargs[key] = "\u200B"
+                                        embeds = []
+                                        for embed in message['embeds']:
+                                            if len(embed):
+                                                kwargs = dict()
+                                                for key, value in embed.items():
+                                                    if not len(value):
+                                                        if key == "description":
+                                                            kwargs[key] = "\u200B"
+                                                        else:
+                                                            kwargs[key] = discord.Embed.Empty
                                                     else:
-                                                        kwargs[key] = discord.Embed.Empty
-                                                else:
-                                                    kwargs[key] = value
+                                                        kwargs[key] = value
 
-                                            logger.debug(kwargs)
-                                            logger.debug(f"making embeds for{kwargs.pop('name')}")
-                                            kwargs['color'] = int(embed['color'][1:], 16)
-                                            embeds.append(discord.Embed(**kwargs))
+                                                logger.debug(kwargs)
+                                                logger.debug(f"making embeds for{kwargs.pop('name')}")
+                                                kwargs['color'] = int(embed['color'][1:], 16)
+                                                embeds.append(discord.Embed(**kwargs))
 
-                                    if msg:
-                                        await msg.edit(
-                                            content=message['content'] if message['content'] else None,
-                                            embeds=embeds
-                                        )
-                                    else:
-                                        msg = await channel.send(
-                                            content=message['content'] if message['content'] else None,
-                                            embeds=embeds
-                                        )
-                                    message['id'] = str(msg.id)  # so that we can check regex patten on it
+                                        if msg:
+                                            await msg.edit(
+                                                content=message['content'] if message['content'] else None,
+                                                embeds=embeds
+                                            )
+                                        else:
+                                            msg = await channel.send(
+                                                content=message['content'] if message['content'] else None,
+                                                embeds=embeds
+                                            )
+                                        message['id'] = str(msg.id)  # so that we can check regex patten on it
+                            else:
+                                logger.debug(f"Invalid channel id {channel_['id']} or the channel not in cache")
                         else:
-                            logger.debug(f"Invalid channel id {channel_['id']} or the channel not in cache")
-
+                            logger.debug(f"No channel in config")
                     for file_path in [self.config_file_path, self.applied_config_path]:
                         with file_path.open('w') as file:
                             json.dump(self.config, file, sort_keys=True, indent=2)
