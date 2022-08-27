e5001a38e2881cc8867415a79bef3e06b618b603
p0lygun <solankivibhakar82@gmail.com>
2022-08-28 02:05:48 +0530

    feat: add HandleTrailingSlashRouter for paths with no slash

--- bot/dashboard/dashboard.py
+++ bot/dashboard/dashboard.py
@@ -8,7 +8,7 @@ import dictdiffer
 import discord
 from loguru import logger
 from ..utils.helper import project_base_path, configs_path, configs_base
-
+from .routers import HandleTrailingSlashRouter
 
 # try:
 #     from ..pgsql import ConnectionWrapper
@@ -80,6 +80,7 @@ try:
         ]
 
         app = FastAPI()
+        router = HandleTrailingSlashRouter()
         discord_client = DiscordOAuthClient(
             client_id,
             client_secret,
@@ -123,11 +124,11 @@ try:
             else:
                 return await call_next(request)
 
-        @app.get("/login")
+        @router.get("/login")
         async def start_login():
             return discord_client.redirect()
 
-        @app.get("/login/callback")
+        @router.get("/login/callback")
         async def finish_login(request: Request, code: str):
             async with discord_client.session(code) as session:
                 user = await session.identify()
@@ -145,7 +146,7 @@ try:
             else:
                 return None
 
-        @app.get("/", response_class=HTMLResponse)
+        @router.get("/", response_class=HTMLResponse)
         async def read_root(request: Request):
             if files := await get_files("global_config"):
                 with open(files[0]) as config_file, open(files[1]) as schema_file:
@@ -156,7 +157,7 @@ try:
                     {"request": request, "schema": schema, "start_val": config},
                 )
 
-        @app.get("/configs/", response_class=HTMLResponse)
+        @router.get("/configs/", response_class=HTMLResponse)
         async def return_config(request: Request, config: str):
             if files := await get_files(config):
                 with open(files[0]) as config_file, open(files[1]) as schema_file:
@@ -169,7 +170,7 @@ try:
                 {"request": request, "schema": schema, "start_val": saved_config},
             )
 
-        @app.get("/user/")
+        @router.get("/user/")
         async def get_user_info(id: int, guild_id: int):
             guild = bot.get_guild(guild_id)
             user = None
@@ -198,15 +199,15 @@ try:
                 else:
                     return {config: "Invalid"}
 
-        @app.get("/raw/configs")
+        @router.get("/raw/configs")
         async def return_config(config: str):
             return await return_file(config, config=True)
 
-        @app.get("/raw/configs/schema")
+        @router.get("/raw/configs/schema")
         async def return_schema(config: str) -> dict:
             return await return_file(config, schema=True)
 
-        @app.get("/get/channel/")
+        @router.get("/get/channel/")
         async def channel_name(id: int):
             channel = bot.get_channel(id)
             if channel:
@@ -214,7 +215,7 @@ try:
             else:
                 return {"channel": {}}
 
-        @app.post("/post/", status_code=201)
+        @router.post("/post/", status_code=201)
         async def save_config(request: Request, config: str):
             if files := await get_files(config):
                 with open(files[0], "w") as file:
@@ -257,6 +258,7 @@ try:
             allow_methods=["*"],
             allow_headers=["*"],
         )
+        app.include_router(router)
 
 except ConnectionError as e:
     logger.critical(f"Unable to connect to Discord. exit error {e}")
